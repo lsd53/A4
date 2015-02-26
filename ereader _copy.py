d900b4719b30feaa44e62df8d7501d80dc56d4de
kevinschaich <schaich.kevin@gmail.com>
2015-02-26 18:40:59 -0500

    added ereader_copy.py

---
+++ ereader _copy.py
@@ -0,0 +1,47 @@
+#! /usr/bin/python 
+import termios, sys, os, hashlib
+#import termios, TERMIOS, sys, os
+TERMIOS = termios
+def getkey():
+        fd = sys.stdin.fileno()
+        old = termios.tcgetattr(fd)
+        new = termios.tcgetattr(fd)
+        new[3] = new[3] & ~TERMIOS.ICANON & ~TERMIOS.ECHO
+        new[6][TERMIOS.VMIN] = 1
+        new[6][TERMIOS.VTIME] = 0
+        termios.tcsetattr(fd, TERMIOS.TCSANOW, new)
+        c = None
+        try:
+                c = os.read(fd, 1)
+        finally:
+                termios.tcsetattr(fd, TERMIOS.TCSAFLUSH, old)
+        return c
+
+if not (os.path.isfile(os.path.expanduser('~')+'/.reader_rc')):
+	file=open(os.path.expanduser('~')+'/.reader_rc','w+b')
+else:
+	file=open(os.path.expanduser('~')+'/.reader_rc','r+')
+
+if len(sys.argv) <= 2:
+	text_location=os.getcwd()+'/'+sys.argv[1]
+	text=open(text_location,'r+')
+	
+	text1=text.readlines()
+	md5_hash=hashlib.md5(text.read()).hexdigest()
+
+	n = 40
+
+	def printlines(n):
+		for s in text1[i:i+n]:
+			print s
+
+	i=0
+
+	printlines(n)
+	while(getkey()!='q'):
+		while(getkey()=='n'):
+			printlines(n)
+			i=i+n
+		while(getkey()=='p'):
+			i=i-n
+			printlines(n)
