34572fb565937b74a33bcc5afe938e6a014fbfca
kevinschaich <schaich.kevin@gmail.com>
2015-02-26 18:52:52 -0500

    ereader_copy updated

--- ereader_copy.py
+++ ereader_copy.py
@@ -22,7 +22,7 @@ if not (os.path.isfile(os.path.expanduser('~')+'/.reader_rc')):
 else:
 	file=open(os.path.expanduser('~')+'/.reader_rc','r+')
 
-if len(sys.argv) <= 2:
+if len(sys.argv) == 2:
 	text_location=os.getcwd()+'/'+sys.argv[1]
 	text=open(text_location,'r+')
 	
@@ -39,9 +39,13 @@ if len(sys.argv) <= 2:
 
 	printlines(n)
 	while(getkey()!='q'):
-		while(getkey()=='n'):
-			printlines(n)
+		printlines(n)
+		while(getkey()==pus'n'):
 			i=i+n
+			printlines(n)
 		while(getkey()=='p'):
 			i=i-n
 			printlines(n)
+	print "Exiting..."
+else:
+	print "Please enter a filename"
