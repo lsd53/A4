12812c7818362079b1a65445342762ac4f848621
Luis Gustavo De Medeiros <lsd53@corenll.edu>
2015-03-04 20:23:20 -0500

    Recent commit

--- ereader.py
+++ ereader.py
@@ -5,32 +5,90 @@ if not (os.path.isfile(os.path.expanduser('~')+'/.reader_rc')):
 else:
 	file=open(os.path.expanduser('~')+'/.reader_rc','r+')
 
+file_lines=file.readlines()
+file.close()
+
+
+
+file=open(os.path.expanduser('~')+'/.reader_rc','w+b')
 if len(sys.argv) <= 2:
 	text_location=os.getcwd()+'/'+sys.argv[1]
 	text=open(text_location,'r+')
 	
 	text1=text.readlines()
 	md5_hash=hashlib.md5(text.read()).hexdigest()
-	if md5_hash not in [s.split(',')[0] for s in file ]:
-		i=0
-		key=raw_input()
-		while(key!='q'):
-			for s in text1[i:i+40]:
-				print s
-			key=raw_input()
-			i=i+40
-		file.write(md5_hash+str(i))
+	page=40
+	text.close()
+else:
+	
+	page=int(sys.argv[2])
+	text_location=os.getcwd()+'/'+sys.argv[3]
+	text=open(text_location,'r+')
+	
+	text1=text.readlines()
+	md5_hash=hashlib.md5(text.read()).hexdigest()
 
-	else:
-		lines_read='0'
-		for line in file:
-			if md5_hash==line.split(',')[0]:
-				lines_read=line.split(',')[1]
-		i=int(lines_read)
-		key=raw_input()
-		while(key!='q'):
-			for s in text1[i:i+40]:
+	text.close()
+
+
+print md5_hash
+if md5_hash not in [s.split(',')[0] for s in file_lines]:
+	i=page
+	for s in text1[0:page]:
+		print s
+	key=raw_input()
+	while(key!='q'):
+		os.system('clear')
+		if key=='n':
+
+			for s in text1[i:i+page]:
 				print s
-			key=raw_input()
-			i=i+40
+			i=i+page
+		if key=='p':
+			i=i-page
+			for s in text1[i-page:i]:
+				print s
+			
+		key=raw_input()
+	
+	file.write(md5_hash+','+str(i))
+
+else:
+	lines_read='0'
+	for line in file_lines:
+		
+		if md5_hash==line.split(',')[0]:
+			lines_read=line.split(',')[1]
+
+	i=int(lines_read)
+	for s in text1[i:i+page]:
+		print s
+	key=raw_input()
+	while(key!='q'):
+		os.system('clear')
+		if key=='n':
+			for s in text1[i:i+page]:
+				print s
+			i=i+page
+		if key=='p':
+			i=i-page
+			for s in text1[i-page:i]:
+				print s
+			
+
+		key=raw_input()
+		
+	for x in xrange(len(file_lines)):
+		if md5_hash==line.split(',')[0]:
+			file_lines[x]=line.split(',')[0]+','+str(i)
+		file.write(file_lines[x])
+
+os.system('clear')
+
+file.close()
+
+
+	
+	
+
 
